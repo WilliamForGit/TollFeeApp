436840a714cfa91e2c34fae03a33457ab609f908
WilliamForGit <haiou.xiao@gmail.com>
2023-09-23 15:42:36 +0200

    first version of CheckTollFeeByTimeRange

---
+++ Application/Services/CheckTollFeeByTimeRange.cs
@@ -0,0 +1,62 @@
+﻿using Domain.JSONSettingEntities;
+using Infrastructure.JSONSettingsService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Services
+{
+    public static class CheckTollFeeByTimeRange
+    {
+        static readonly List<TollFeeTPRange> tollFeeRanges = new();
+
+        private static void LoadTollFeeData()
+        {
+            try
+            {
+                var timePointsData = new GetJsonSettingsData<DayTollFeeTimePoints>();
+
+                DayTollFeeTimePoints tollFeeTRData = timePointsData.ReadJsonSettingsData("TollFeeTimePoints");
+
+                foreach (var timePoint in tollFeeTRData.TollFeeTimePoints)
+                {
+                    foreach (var timeRangeStr in timePoint.TimePoints)
+                    {
+                        // Split the time range into start and end points using '-' as the separator.
+                        var timeRangeParts = timeRangeStr.Split('-');
+                        if (timeRangeParts.Length == 2 &&
+                            TimeSpan.TryParse(timeRangeParts[0], out TimeSpan startTime) &&
+                            TimeSpan.TryParse(timeRangeParts[1], out TimeSpan endTime))
+                        {
+                            tollFeeRanges.Add(new TollFeeTPRange
+                            {
+                                FeeAmount = timePoint.FeeAmount,
+                                StartTime = startTime,
+                                EndTime = endTime
+                            });
+                        }
+                        else
+                        {
+                            // Handle invalid time range format.
+                            Console.WriteLine($"Invalid time range format: {timeRangeStr}");
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // Handle any other exceptions that may occur during parsing.
+                Console.WriteLine($"Error processing time range. Error: {ex.Message}");
+            }
+        }
+    }
+
+    public class TollFeeTPRange
+    {
+        public int FeeAmount { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
