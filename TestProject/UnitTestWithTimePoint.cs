e7e957407cdfa48f8c363e9ab286cdc0e2837f48
WilliamForGit <haiou.xiao@gmail.com>
2023-09-23 16:09:52 +0200

    Unit test class UnitTestWithTimePoint

---
+++ TestProject/UnitTestWithTimePoint.cs
@@ -0,0 +1,34 @@
+﻿using Application.Services;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTestWithTimePoint
+    {
+        [TestMethod]
+        public void TestTimePointForFee()
+        {
+            var tollVehicleObj = new TollVehicle
+            {
+                VehicleId = "QWE789",
+                VehicleType = "Car",
+                VehicleDateTimes = new List<DateTime>
+                {
+                    DateTime.Parse("2013-09-05 17:56:00")
+                }
+            };
+
+            int result = CheckTollFeeByTimeRange.GetTollFeeByTimeRange(tollVehicleObj.VehicleDateTimes.FirstOrDefault());
+
+            Assert.AreEqual(13, result);
+
+            Console.WriteLine($"Unit Test case for {tollVehicleObj.VehicleId} for a timepoint {tollVehicleObj.VehicleDateTimes.FirstOrDefault()} with fee: {result}");
+        }
+    }
+}
